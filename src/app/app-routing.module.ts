bfe8ca79be2f4522b17f874294d5328eb6d9310a
Adiasto <30463110+Adiasto@users.noreply.github.com>
2019-01-14 14:22:10 +0530

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { LoginService } from './auth/login.service';
+import { LoggedIn } from './auth/auth-gaurd.service';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'drivers',
+    loadChildren: './drivers/drivers.module#DriversModule',
+    canActivate: [LoggedIn]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
+import { LoggedIn } from './auth/auth-gaurd.service';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule
   ],
-  providers: [],
+  providers: [LoggedIn],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
